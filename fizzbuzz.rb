41ac6407a07d204319bc35cd1857f2cf7cadfd55
Ben Francisco <bpfranci@math.tamu.edu>
2019-08-18 21:20:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,3 +2,23 @@
 # See README.md for instructions on how to do this
 
 #
+
+# fizzbuzz: Single argument version of the fizzbuzz problem.
+# =>  int - an integr value to be read in and processed.
+# <=  int - if the integer is neither divisble by 3 nor 5.
+# <=  fizz - if the integer is divisible by 3.
+# <=  buzz - if the integer is divisible by 5.
+# <=  fizzbuzz - if the integer is divisible by both 3 and 5.
+def fizzbuzz(int)
+  if int % 3 == 0
+    if int % 5 == 0
+      return "FizzBuzz"
+    else
+      return "Fizz"
+    end
+  if int % 5 == 0
+    return "Buzz"
+  end
+    return int
+  end
+    
